babe67be184dfc22081ae35a6d9b5d446cd613a4
ExtraPolite <imextrapolite2@gmail.com>
2024-08-22 15:22:03 -0400

    try to recreate recipe page with react

---
+++ recipes/src/App.jsx
@@ -0,0 +1,13 @@
+import Lasagna from './Lasagna';
+
+function App() {
+  return (
+    <>
+      <h1>Odin Recipes</h1>
+
+      <Lasagna />
+    </>
+  );
+}
+
+export default App;
---
+++ recipes/src/Lasagna.jsx
@@ -0,0 +1,9 @@
+export default function Lasagna() {
+  return (
+    <div className="hide">
+      <h1>Lasagna Recipe</h1>
+
+      <img src="assets/react.svg" alt="" />
+    </div>
+  );
+}
